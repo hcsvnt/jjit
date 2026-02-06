14618e73be6ba6271af46c35c8b0347038f2554d
Mateusz Hallala <hallala.work@gmail.com>
2026-02-07 00:12:52 +0100

    update: home page doc

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic';
+
 import styles from './page.module.css';
 import DateElement from './register_form/DateElement';
 import RegisterForm from './register_form/register_form';
@@ -8,6 +10,12 @@ import RegisterForm from './register_form/register_form';
  * Fetches the current date server-side and renders the registration form,
  * passing the date into the form header.
  *
+ *
+ * @remarks
+ * Since this component renders the DateElement, which performs a server-side fetch,
+ * we need to force the entire page to be dynamic.
+ * This is still very beneficial, though, because we get the outer shell instantly,
+ * with suspense waiting only for the date element, and not the entire form.
  *
  */
 export default async function Home() {
